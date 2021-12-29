a9eb1f9e9fab0157467e1217c6669413d8bf6cad
Oliver <olivercai@uchicago.edu>
2021-12-29 01:36:47 -0500

    added admin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "oliver", password: "secret", except: [:index, :show]
 
   def index
     @articles = Article.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index', to: 'home#index'
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users, controllers: { registrations: "users/registrations" }
   get 'signin/index', to: 'signin#index'
   root to: 'home#index'
   resources :articles do 
     resources :comments
   end
-  post 'signin/signinform', :to => 'signin#loginform', as: :loginform
-  post 'signin/signupform', :to => 'signin#signupform', as: :signupform
+  get 'signin/signinform', :to => 'signin#loginform', as: :loginform
+  get 'signin/signupform', :to => 'signin#signupform', as: :signupform
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
